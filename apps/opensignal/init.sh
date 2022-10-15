0d67bcc015278f348ce022f51955beb977c79f97
Álvaro <alvaro.pastorsanchez@opensignal.com>
2022-10-15 09:27:50 +0100

    update BLAS exports for python

--- apps/opensignal/init.sh
+++ apps/opensignal/init.sh
@@ -7,22 +7,30 @@ alias dap="cd ~/opensignal/data-aggregation-pipeline/"
 # FLAGS
 OPENBLAS="$(brew --prefix openblas)"
 export OPENBLAS
-#export LDFLAGS="-L/opt/homebrew/opt/lapack/lib"
-#export CPPFLAGS="-I/opt/homebrew/opt/lapack/include"
-#export PKG_CONFIG_PATH="/opt/homebrew/opt/lapack/lib/pkgconfig"
-#export NPY_DISTUTILS_APPEND_FLAGS=1
-#export SYSTEM_VERSION_COMPAT=1
-#export CFLAGS="-falign-functions=8"
+export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
+export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1
+export LDFLAGS="-L/opt/homebrew/opt/lapack/lib -L/opt/homebrew/opt/openssl/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/lapack/include"
+export CFLAGS="-I/opt/homebrew/opt/lapack/include -falign-functions=8"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/lapack/lib/pkgconfig"
+export NPY_DISTUTILS_APPEND_FLAGS=1
+export SYSTEM_VERSION_COMPAT=1
+# export CFLAGS="-falign-functions=8"
 #
 ## Libraries
-#export LAPACK=/opt/homebrew/opt/lapack/lib/liblapack.dylib
-#export BLAS=/opt/homebrew/opt/openblas/lib/libopenblas.dylib
+export LAPACK=/opt/homebrew/opt/lapack/lib/liblapack.dylib
+export BLAS=/opt/homebrew/opt/openblas/lib/libopenblas.dylib
 
 function setup_repo() {
   # Ensure running in virtualenv
   git submodule update --init --remote
+  if [ -z "$VIRTUAL_ENV" ]; then
+    echo "No virtual env!! Create one with 'pyenv virtualenv <python-version> <virtualenv-name>'"
+    return 1
+  fi
   pip install virtualenv
   virtualenv venv
-  ln -fs $(which pip) venv/bin/pip3
+  ln -fs "$VIRTUAL_ENV" venv
   pip3 install -U --extra-index-url https://repo.opnsgnl.net/repository/pypi/simple  devops-ci-tools --upgrade
+  make setup
 }
--- apps/opensignal/install.sh
+++ apps/opensignal/install.sh
@@ -7,7 +7,8 @@ brew install openblas
 brew install gdal
 brew install vault
 
-export OPENBLAS=$(/opt/homebrew/bin/brew --prefix openblas)
+OPENBLAS=$(/opt/homebrew/bin/brew --prefix openblas)
+export OPENBLAS=$OPENBLAS
 export CFLAGS="-falign-functions=8 ${CFLAGS}"
 export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
 export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1
