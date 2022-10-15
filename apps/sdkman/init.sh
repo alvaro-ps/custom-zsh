e7476cd9748d400155d5c2732ed345f833d003ae
Álvaro <alvaro.pastorsanchez@opensignal.com>
2022-10-15 09:27:02 +0100

    use spark3 by default

--- apps/sdkman/init.sh
+++ apps/sdkman/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 function scala2 () {
+  # Deprecated
   sdk use java 8.0.342-amzn
   sdk use scala 2.12.17
   sdk use spark 2.4.7
@@ -9,16 +10,13 @@ function scala2 () {
   export SPARK_VERSION=2
 }
 
-function scala3 () {
-  sdk use java 11.0.16-amzn
-  #sdk use scala 3.2.0
-  sdk use scala 2.12.17
-  sdk use spark 3.2.1
-  export DATA_SCALA_UTILS_VERSION=2.1.9
-  export SCALA_VERSION=2.12
-  export SPARK_VERSION=3
-  export PYSPARK_PYTHON=$(which python)
-}
+export DATA_SCALA_UTILS_VERSION=2.1.17
+export SCALA_VERSION=2.12
+export SPARK_VERSION=3
+SPARK_DETAILED_VERSION=3.2.1
+export SPARK_HOME="$(sdk home spark $SPARK_DETAILED_VERSION)"
+export PYSPARK_PYTHON=$(which python)
+alias spark="spark-shell --packages org.apache.spark:spark-hadoop-cloud_${SCALA_VERSION}:${SPARK_DETAILED_VERSION}"
 
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
--- apps/sdkman/install.sh
+++ apps/sdkman/install.sh
@@ -7,3 +7,8 @@
 # sdk install scala 2.12.17
 sdk install spark 2.4.7
 sdk install spark 3.2.1
+
+sdk default java 11.0.16-amzn
+#sdk use scala 3.2.0
+sdk default scala 2.12.17
+sdk default spark 3.2.1
