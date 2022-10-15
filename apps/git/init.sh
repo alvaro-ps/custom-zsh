e19fe8e6944e885925f6a7aa51fb841ad2701136
Álvaro <alvaro.pastorsanchez@opensignal.com>
2022-10-15 09:29:08 +0100

    add git init

--- apps/git/init.sh
+++ apps/git/init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 alias push='br=$(git branch | grep \* | cut -d " " -f2); git push origin $br'
 alias pull='br=$(git branch | grep \* | cut -d " " -f2); git pull origin $br'
+
+alias unstage='git checkout --'
