d770fc82ead3c479c44aa77457ea99dad209be5d
Álvaro <alvaro.pastorsanchez@opensignal.com>
2022-10-28 13:32:38 +0100

    push and pull as functions

--- apps/git/init.sh
+++ apps/git/init.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-alias push='br=$(git branch | grep \* | cut -d " " -f2); git push origin $br'
-alias pull='br=$(git branch | grep \* | cut -d " " -f2); git pull origin $br'
+
+function get_branch () {
+  branch=$(git branch | grep "\*" | cut -d " " -f2)
+}
+
+function push () {
+  get_branch
+  git push origin "${branch}"
+}
+
+function pull (){
+  get_branch
+  git pull origin "${branch}"
+}
 
 alias commit='git commit -m'
 alias unstage='git checkout --'
