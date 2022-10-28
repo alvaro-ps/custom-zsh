f9f0b6c4f76d8d8b7e6d462d9338f5f78f7fa1c3
Álvaro <alvaro.pastorsanchez@opensignal.com>
2022-10-28 13:13:12 +0100

    add git aliases

--- apps/git/init.sh
+++ apps/git/init.sh
@@ -3,3 +3,6 @@ alias push='br=$(git branch | grep \* | cut -d " " -f2); git push origin $br'
 alias pull='br=$(git branch | grep \* | cut -d " " -f2); git pull origin $br'
 
 alias unstage='git checkout --'
+alias gco='git checkout'
+alias gd='git branch -D'
+alias gc='git commit -m'
