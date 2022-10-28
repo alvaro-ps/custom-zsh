d1e87974cf394917a649fae5ba62d8fcbd35490d
Álvaro <alvaro.pastorsanchez@opensignal.com>
2022-10-28 13:28:31 +0100

    update aliases

--- apps/git/init.sh
+++ apps/git/init.sh
@@ -2,7 +2,7 @@
 alias push='br=$(git branch | grep \* | cut -d " " -f2); git push origin $br'
 alias pull='br=$(git branch | grep \* | cut -d " " -f2); git pull origin $br'
 
+alias commit='git commit -m'
 alias unstage='git checkout --'
-alias gco='git checkout'
-alias gd='git branch -D'
-alias gc='git commit -m'
+alias checkout='git checkout'
+alias branch_del='git branch -D'
