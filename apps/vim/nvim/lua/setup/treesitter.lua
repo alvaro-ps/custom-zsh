3ade5fa0a8df6c656bfdb6efd8bc5f434c9358f4
Álvaro <alvaro.pastorsanchez@opensignal.com>
2022-10-28 13:26:31 +0100

    add bash and sql to treesitter

--- apps/vim/nvim/lua/setup/treesitter.lua
+++ apps/vim/nvim/lua/setup/treesitter.lua
@@ -9,7 +9,7 @@ function M.setup()
 
   require('nvim-treesitter.configs').setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "lua", "python", "scala" },
+    ensure_installed = { "lua", "python", "scala", "bash", "sql"},
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
@@ -17,6 +17,10 @@ function M.setup()
     -- Automatically install missing parsers when entering buffer
     -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
+
+    indent = {
+      enable = true
+    },
 
     highlight = {
       -- `false` will disable the whole extension
