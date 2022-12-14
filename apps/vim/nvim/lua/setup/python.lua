f2014edd6e04b400cd75b9180566f734be842433
Álvaro <alvaro.pastorsanchez@opensignal.com>
2022-12-14 12:23:01 +0000

    just use default python

--- apps/vim/nvim/lua/setup/python.lua
+++ apps/vim/nvim/lua/setup/python.lua
@@ -2,10 +2,11 @@ local M = {}
 
 function M.setup()
   require('lspconfig').pyright.setup{}
+  --vim.g.python3_host_prog = "~/.pyenv/shims/python"
 
   -- Debug settings if you're using nvim-dap
   local dap = require("dap")
-  local dap_python = require("dap-python").setup("~/.pyenv/shims/python")
+  local dap_python = require("dap-python").setup("python")
 
 end
 
