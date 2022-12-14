aae2e8f8a5868ad41a06d2de095bb44db663fbaa
Álvaro <alvaro.pastorsanchez@opensignal.com>
2022-12-14 11:29:21 +0000

    formatting

--- apps/vim/nvim/lua/setup/treesitter.lua
+++ apps/vim/nvim/lua/setup/treesitter.lua
@@ -9,7 +9,7 @@ function M.setup()
 
   require('nvim-treesitter.configs').setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "lua", "python", "scala", "bash", "sql"},
+    ensure_installed = { "lua", "python", "scala", "bash", "sql" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
