0a119bdb6d162158e503ab3f54efbbdd459eed8c
Álvaro <alvaro.pastorsanchez@opensignal.com>
2023-01-16 15:39:04 +0000

    transparent background true

--- apps/vim/nvim/lua/setup/colorscheme.lua
+++ apps/vim/nvim/lua/setup/colorscheme.lua
@@ -2,7 +2,9 @@ local M = {}
 
 function M.setup()
   vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
-  require("catppuccin").setup()
+  require("catppuccin").setup({
+    transparent_background = true,
+  })
   vim.api.nvim_command "colorscheme catppuccin"
 end
 
