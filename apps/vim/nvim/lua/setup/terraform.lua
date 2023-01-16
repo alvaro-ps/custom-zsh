9cc1615612600b16b615d0d1b287329a3f747a2e
Álvaro <alvaro.pastorsanchez@opensignal.com>
2023-01-16 15:31:49 +0000

    format (not fully sure it works)

--- apps/vim/nvim/lua/setup/terraform.lua
+++ apps/vim/nvim/lua/setup/terraform.lua
@@ -8,7 +8,7 @@ function M.setup()
 
   vim.api.nvim_create_autocmd({"BufWritePre"}, {
     pattern = {"*.tf", "*.tfvars"},
-    callback = vim.lsp.buf.formatting_sync,
+    callback = vim.lsp.buf.format,
   })
 
 end
