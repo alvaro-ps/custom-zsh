56f8495a9091c1ae182ac24b10bd590602eb7d91
Álvaro <alvaro.pastorsanchez@opensignal.com>
2023-04-20 11:44:07 +0100

    use SDKMAN folder

--- apps/sdkman/init.sh
+++ apps/sdkman/init.sh
@@ -12,7 +12,7 @@ function scala2 () {
 
 
 export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
 export DATA_SCALA_UTILS_VERSION=$(head ~/opensignal/data-aggregation-pipeline/DATA_SCALA_UTILS_VERSION)
 export SCALA_VERSION=2.12
