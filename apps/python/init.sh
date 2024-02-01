63d10c6007361a906169b703ab1f0cd9b7600398
Álvaro <alvaro.pastorsanchez@opensignal.com>
2024-02-01 12:24:27 +0000

    add pytree

--- apps/python/init.sh
+++ apps/python/init.sh
@@ -2,3 +2,5 @@ export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+alias pytree="tree -I .pyc -I __pycache__"
