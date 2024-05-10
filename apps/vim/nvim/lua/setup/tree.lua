1855e021fbff1d4b55424c4a2c24b24a071d51fa
Álvaro <alvaro.pastorsanchez@opensignal.com>
2024-05-10 08:29:32 +0100

    update nvim tree

--- apps/vim/nvim/lua/setup/tree.lua
+++ apps/vim/nvim/lua/setup/tree.lua
@@ -6,14 +6,18 @@ function M.setup()
   vim.g.loaded_netrwPlugin = 1
 
   require("nvim-tree").setup({
-    sort_by = "case_sensitive",
+    sort = {
+      sorter = "case_sensitive",
+    },
     view = {
       adaptive_size = true,
+      --[[*
       mappings = {
         list = {
           { key = "u", action = "dir_up" },
         },
       },
+      *--]]
     },
     renderer = {
       group_empty = true,
