2a75a6aa0ad2dd9aa8d7281f08e1188b9b15ba21
Álvaro <alvaro.pastorsanchez@opensignal.com>
2024-09-03 19:36:41 +0100

    use mocha

--- apps/vim/nvim/lua/setup/colorscheme.lua
+++ apps/vim/nvim/lua/setup/colorscheme.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup()
-  vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
+  vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
   require("catppuccin").setup()
   vim.api.nvim_command "colorscheme catppuccin"
 end
