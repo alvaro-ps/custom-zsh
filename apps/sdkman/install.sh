46e48b07b4f3c60df0d8378930f9376467600fb8
Álvaro <alvaro.pastorsanchez@opensignal.com>
2025-03-31 16:14:24 +0200

    scala version

--- apps/sdkman/install.sh
+++ apps/sdkman/install.sh
@@ -12,7 +12,7 @@ sdk default java 17.0.10-amzn
 #sdk use scala 3.2.0
 #sdk default scala 2.12.17
 #sdk default spark 3.2.1
-sdk default scala 2.13.8
+sdk default scala 2.13.15
 # Install manually and have sdkman point to it with 
 # `sdk install spark 3.5.4-scala2.13 ~/Downloads/spark-3.5.4-scala2.13`
 sdk default spark 3.5.4-scala2.13
