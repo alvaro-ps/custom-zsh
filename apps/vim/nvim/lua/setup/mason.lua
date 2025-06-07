d2e15bd6f90abb4d8591ab73ef8f0bdd3d24fb60
Álvaro <alvaro.pastorsanchez@opensignal.com>
2025-06-07 23:48:13 +0200

    only enable servers installed by mason

--- apps/vim/nvim/lua/setup/mason.lua
+++ apps/vim/nvim/lua/setup/mason.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.setup()
   require('mason').setup()
   require('mason-lspconfig').setup {
+    automatic_enable = false,
     ensure_installed = {
       "lua_ls",
       "cucumber_language_server",
