e1f6606cb99e67c6449f3a0b9a45b48f5bd57965
Álvaro <alvaro.pastorsanchez@opensignal.com>
2026-01-19 18:16:50 +0100

    setup macchiato

--- apps/vim/nvim/lua/setup/colorscheme.lua
+++ apps/vim/nvim/lua/setup/colorscheme.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup()
-  vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+  vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
   require("catppuccin").setup({
     term_colors = true,
     transparent_background = true,
